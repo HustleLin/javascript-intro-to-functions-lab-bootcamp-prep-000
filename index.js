fc433c68caa9252431adf07d7925aad8f328e3eb
Jonathan Lin <jlin8293@gmail.com>
2018-02-06 03:05:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
   { return "YES INDEED!" }
 
-  else (string === 'I love you, Grandma.')
+  if (string === 'I love you, Grandma.')
   { return "I love you, too." }
 }

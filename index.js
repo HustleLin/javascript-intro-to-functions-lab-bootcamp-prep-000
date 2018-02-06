985e66667c2bee2cf6d06343b6811f6f04815aa1
Jonathan Lin <jlin8293@gmail.com>
2018-02-06 02:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ lowercase.toLowerCase() === lowercase
 function sayHiToGrandma(string) {
   if (`${string} === ${lowercase}`)
   return "I can't hear you!"
-  else (`${string} === ${uppercase}`)
+  if (`${string} === ${uppercase}`)
   return "YES INDEED!"
 }
